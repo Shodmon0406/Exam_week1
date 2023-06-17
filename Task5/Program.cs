06e887f3ddc20ac7c4c446af80970a6a15426605
Shodmon0406 <shodmoninoyatzoda@yandex.ru>
2023-06-17 17:47:25 +0500

    NewCommit

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,21 +4,31 @@ for (int i = 0; i < n; i++)
 {
     arr[i] = int.Parse(Console.ReadLine());
 }
-int min = arr[0], max = arr[0], a = 0, b = 0;;
+int min = arr[0], max = arr[0], a = 0, b = 0; ;
 for (int i = 0; i < n; i++)
 {
     if (arr[i] < min)
     {
         min = arr[i];
         a = i;
-    } 
+    }
     if (arr[i] > max)
     {
         max = arr[i];
         b = i;
-    } 
+    }
 }
-for (int i = a; i <= b; i++)
+if (a < b)
 {
-    System.Console.Write(arr[i] + " ");
+    for (int i = a; i <= b; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
+else
+{
+    for (int i = a; i >= b; i--)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
 }
